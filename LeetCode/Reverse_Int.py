09b10e35027bf88988aec968945c4027a5d97bb2
Navaneeth_Krishna <95477780+navaneethkrishna11@users.noreply.github.com>
2024-07-27 09:10:11 +0530

    Create Reverse_Int.py

---
+++ LeetCode/Reverse_Int.py
@@ -0,0 +1,36 @@
+"""
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+"""
+class Solution:
+    def reverse(self, x: int) -> int:
+        is_negative = x<0
+        x=abs(x)
+
+        rev=0
+        while x>0:
+            rev=(rev*10)+x%10
+            x=x//10
+        
+        if is_negative ==True:
+            rev=-rev
+        
+        if rev<-2**31 or rev > 2**31-1:
+            return 0
+        return rev
+        
