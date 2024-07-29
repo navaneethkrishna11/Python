70e378b401471f654bbc61391457fe77bd3e08f6
Navaneeth_Krishna <95477780+navaneethkrishna11@users.noreply.github.com>
2024-07-30 01:11:41 +0530

    Create IntegerToRoman.py

---
+++ LeetCode/IntegerToRoman.py
@@ -0,0 +1,24 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman_mapping={
+            1000:"M",
+            900:'CM',
+            500:'D',
+            400:'CD',
+            100:'C',
+            90:'XC',
+            50:'L',
+            40:'XL',
+            10:'X',
+            9:'IX',
+            5:'V',
+            4:'IV',
+            1:'I'
+        }
+        roman_numeral=""
+        for value,symbol in roman_mapping.items():
+            while num>=value:
+                roman_numeral += symbol
+                num -= value
+        
+        return roman_numeral
